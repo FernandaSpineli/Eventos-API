2264848eee37b72339ecf541269c9b49bdb6394f
FernandaSpineli <fernandaspinx@gmail.com>
2022-10-28 13:50:30 -0300

    conectando com bd SQLite e alterando modelos

--- eventos/agenda/models.py
+++ eventos/agenda/models.py
@@ -5,22 +5,9 @@ from django.db import models
 class Categoria(models.Model):
     nome = models.CharField(max_length=100, unique=True)
 
-
 class Evento(models.Model):
     nome = models.CharField(max_length=100)
     categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True)
     local = models.CharField(max_length=100, blank=True)
     link = models.CharField(max_length=100, blank=True)
-    
-    def __init__(self, nome, categoria, local=None, link=None):
-        self.nome = nome
-        self.categoria = categoria
-        self.local = local
-        self.link = link
         
-aula_python = Evento('Aula de Python', 'Backend', 'Rio de Janeiro')
-aula_js = Evento('Aula de JavaScript', 'Fullstack', link='https://tamarcado.com/')
-eventos = [
-    aula_python,
-    aula_js
-]
--- eventos/agenda/views.py
+++ eventos/agenda/views.py
@@ -3,14 +3,16 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.template import loader
 
-from agenda.models import eventos
-
 # Create your views here.
 def index(request):
     return HttpResponse('Olá mundo!')
 
 def exibir_evento(request):
-    evento = eventos[1]
+    evento = {
+        'nome': 'Teste',
+        'categoria': 'Categoria A',
+        'local': 'São Paulo'
+    }
     return render(request=request, context={'evento': evento}, template_name='agenda/exibir_evento.html')
 #    template = loader.get_template('agenda/exibir_evento.html')
 #    rendered_template = template.render(context={'evento': evento}, request=request)
