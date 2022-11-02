e49bc273fb66b78da94f6fb9d6ff15381cadfa5e
FernandaSpineli <fernandaspinx@gmail.com>
2022-11-02 13:35:03 -0300

    adicionando admin e alterando rota index

--- eventos/agenda/models.py
+++ eventos/agenda/models.py
@@ -4,6 +4,9 @@ from django.db import models
 
 class Categoria(models.Model):
     nome = models.CharField(max_length=100, unique=True)
+    
+    def __str__(self):
+        return f'{self.nome} <{self.id}>'
 
 class Evento(models.Model):
     nome = models.CharField(max_length=100)
--- eventos/agenda/views.py
+++ eventos/agenda/views.py
@@ -3,9 +3,17 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.template import loader
 
+from agenda.models import Evento
+
 # Create your views here.
-def index(request):
-    return HttpResponse('Olá mundo!')
+def listar_eventos(request):
+    eventos = Evento.objects.all()
+    return render(
+        request=request,
+        context={'eventos': eventos},
+        template_name='agenda/listar_eventos.html'
+    )
+
 
 def exibir_evento(request):
     evento = {
@@ -14,7 +22,5 @@ def exibir_evento(request):
         'local': 'São Paulo'
     }
     return render(request=request, context={'evento': evento}, template_name='agenda/exibir_evento.html')
-#    template = loader.get_template('agenda/exibir_evento.html')
-#    rendered_template = template.render(context={'evento': evento}, request=request)
-#    return HttpResponse(rendered_template)
+
     
